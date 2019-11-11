e57ee2b0025c9dd5f694036ae3b2b41a54591538
Niall Quigley <niallquigley92@gmail.com>
2019-11-11 15:06:52 +0000

    First render test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -9,3 +9,9 @@ test('renders without crashing', () => {
   const app = findByTestAttr(wrapper, "app");
   expect(app.length).toBe(1);
 });
+
+test('text hello world appears', () => {
+  const wrapper = shallow(<App />);
+  const textElement = findByTestAttr(wrapper, 'hello');
+  expect(textElement.text()).toEqual('Hello world');
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component<{}, { something: string }> {
@@ -13,15 +12,7 @@ class App extends React.Component<{}, { something: string }> {
   render() {
     return (
       <div data-test="app" className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
+        <div data-test="hello">Hello world</div>
       </div>
     );
   }
