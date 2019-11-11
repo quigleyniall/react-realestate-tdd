e311237604d9e529995540ffd5e83a2ce96bf293
Niall Quigley <niallquigley92@gmail.com>
2019-11-11 18:15:48 +0000

    Server

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const api = express();
+
+api.use(cors())
+api.use(bodyParser.urlencoded({ extended: true }));
+api.use(bodyParser.json({ limit: '5mb' }));
+
+api.get('/:location', async (req, res) => {
+  const { location } = req.params;
+  const request = await axios.get(`
+    https://api.nestoria.co.uk/api?encoding=json&pretty=1&action=search_listings&country=uk&listing_type=buy&place_name=${location}
+  `);
+  const response = await request.data;  
+  return res.json(response);
+})
+
+api.listen(8080, () => {
+  console.log("Running")
+})
