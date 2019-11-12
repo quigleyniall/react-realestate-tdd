8fe9b6158b33b9f6f628378edfc034e815fe8bfa
Niall Quigley <niallquigley92@gmail.com>
2019-11-12 21:38:17 +0000

    Add timeout to wait on api request to complete

--- client/cypress/integration/SearchBar/search_bar.test.js
+++ client/cypress/integration/SearchBar/search_bar.test.js
@@ -1,8 +1,12 @@
+import { sampleResponse } from '../../../src/test/sampleResponse';
+
 describe('test search bar', () => {
   it('searches nestoria api and shows result on page', () => {
     const location = 'London';
     cy.visit('http://localhost:3000');
     cy.get("[data-test='search-bar']").type(location);
+    cy.get("[data-test='search-button']").click();
+    cy.wait(10000);
+    cy.contains(location)
   });
-
 })
