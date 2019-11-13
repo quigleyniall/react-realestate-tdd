0770520d2f60c91f2dc9d8735f8cb8a652553e09
Niall Quigley <niallquigley92@gmail.com>
2019-11-13 15:06:21 +0000

    Create button component and tests

---
+++ client/src/components/Button/Button.test.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Button from './Button';
+import { findByTestAttr, checkProps } from '../../test/testUtils';
+
+
+const onPressMockFn = jest.fn();
+
+const defaultProps = {
+  text: 'Click me',
+  onPress: onPressMockFn,
+  btnClass: 'primary',
+  active: false
+}
+
+const setup = (props = {}) => {
+  const setupProps = {...defaultProps, ...props}
+  return shallow(<Button {...setupProps} />);
+}
+
+describe('button test', () => {
+  let wrapper;
+  let buttonElement;
+
+  beforeEach(() => {
+    wrapper = setup();
+    buttonElement = findByTestAttr(wrapper, 'button');
+  })
+  test('button renders without error', () => {
+    expect(buttonElement.length).toBe(1);
+  });
+
+  test('button runs onClick function', () => {
+    buttonElement.simulate('click');
+    const onPressMockFnCallCount = onPressMockFn.mock.calls.length;
+    expect(onPressMockFnCallCount).toBe(1);
+  });
+
+  test('check props sent to button', () => {
+    const expectedProps = { onPress: jest.fn(), text: 'click me', btnClass: 'primary' }; 
+    checkProps(Button, expectedProps);
+  });
+
+  test('renders text click me', () => {
+    expect(buttonElement.text()).toBe('Click me');
+  })
+
+  test('renders appropaite className', () => {
+    expect(buttonElement.hasClass('btn-primary')).toBe(true);
+  })
+
+  test('button has active class when active is true', () => {
+    const wrapper = setup({ active: true });
+    const buttonElement = findByTestAttr(wrapper, 'button');
+    expect(buttonElement.hasClass('active')).toBe(true);
+  });
+
+  test('button doesnt have active class when active is false', () => {
+    expect(buttonElement.hasClass('active')).not.toBe(true);
+  })
+})
---
+++ client/src/components/Button/Button.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+interface IProps {
+  onPress: () => void;
+  text: string;
+  active?: boolean;
+  btnClass: string;
+}
+
+const Button = ({ onPress, text, active, btnClass }: IProps) => (
+  <button
+    data-test="button"
+    className={active ? `btn btn-${btnClass} active` : `btn btn-${btnClass}`}
+    onClick={onPress}
+  >
+    {text}
+  </button>
+);
+
+Button.propTypes = {
+  onPress: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+  active: PropTypes.bool,
+  btnClass: PropTypes.string.isRequired
+};
+
+export default Button;
