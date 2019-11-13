40c9f72b7d7bd576bcde172920babb595083cfa4
Niall Quigley <niallquigley92@gmail.com>
2019-11-13 19:07:07 +0000

    Fix cypress tests for new routing

--- client/cypress/integration/SearchBar/search_bar.test.js
+++ client/cypress/integration/SearchBar/search_bar.test.js
@@ -1,12 +1,36 @@
-import { sampleResponse } from '../../../src/test/sampleResponse';
-
 describe('test search bar', () => {
-  it('searches nestoria api and shows result on page', () => {
-    const location = 'London';
+  const location = 'Manchester';
+  
+  it('navigates to rent listings page when search button is clicked', () => {
     cy.visit('http://localhost:3000');
+    cy.get("[data-test='rent-button']").click();
     cy.get("[data-test='search-bar']").type(location);
     cy.get("[data-test='search-button']").click();
-    cy.wait(10000);
-    cy.contains(location)
+    cy.url().should('include', `/listings/rent/${location}`)
+  });
+  it('navigate to buy listins page when search button is clicked', () => {
+    cy.visit('http://localhost:3000');
+    cy.get("[data-test='buy-button']").click();
+    cy.get("[data-test='search-bar']").type(location);
+    cy.get("[data-test='search-button']").click();
+    cy.url().should('include', `/listings/buy/${location}`)
+  });  
+})
+
+describe('card navigation', () => {
+  it('navigates to buy listings page when first card is clicked', () => {
+    cy.visit('http://localhost:3000');
+    cy.get("[data-test='card-button']").first().click();
+    cy.url().should('include', '/listings/buy/london')
+  });
+  it('navigates to rent listings page when last card is clicked', () => {
+    cy.visit('http://localhost:3000');
+    cy.get("[data-test='card-button']").last().click();
+    cy.url().should('include', '/listings/rent/london')
+  });
+  it('navigates to buy listings page when second card is clicked', () => {
+    cy.visit('http://localhost:3000');
+    cy.get("[data-test='card-button']").eq(1).click();
+    cy.url().should('include', '/listings/buy/london')
   });
 })
